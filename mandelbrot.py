29edabdba94c52372eebeb152da1e405bd7cd285
jasoc <paridegiunta@gmail.com>
2021-11-21 19:17:37 +0100

    Now --iterate flag iterate over decimal value with a 0.05 step

--- mandelbrot.py
+++ mandelbrot.py
@@ -2,6 +2,8 @@ import os
 import sys
 import time
 
+import numpy as np
+
 
 def mandelbrot(exp):
 
@@ -50,16 +52,19 @@ def main():
 
         if sys.argv[1] == "-i" or sys.argv[1] == "--iterate":
             iterate = True
-            exp = int(sys.argv[2])
+            exp = float(sys.argv[2])
         else:
-            exp = int(sys.argv[1])
+            exp = float(sys.argv[1])
 
     if iterate:
-        for i in range(exp + 1):
-            mandelbrot(i)
-            time.sleep(0.15)
+        step = 0.05
+        arr = np.arange(0, exp + step, step)
 
-            if i != exp:
+        for i in arr:
+            mandelbrot(i)
+            time.sleep(0.03)
+
+            if i != arr[-1]:
                 os.system('cls')
     else:
         mandelbrot(exp)
