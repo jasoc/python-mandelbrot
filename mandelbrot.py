e4a2ad4f05874fa2f99880f0265d4edaca5a1b51
Jasoc <paridegiunta@gmail.com>
2022-10-29 16:38:05 +0200

    Some random improvements

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import time
 
@@ -9,6 +10,8 @@ def mandelbrot(exp):
 
     max = 4
     it = 10
+    chardnum = 2
+    chararr = '£$%&ç@#*+?'
 
     x_axis = [_ * 0.05 for _ in range(-max * 7, max * 7)]
     y_axis = [_ * 0.05 for _ in range(-max * 10, max * 10)]
@@ -38,13 +41,13 @@ def mandelbrot(exp):
                     break
 
             if abs(res) <= max / 2:
-                conc += "**"
+                conc += f"{random.choice(chararr)}" * chardnum
             else:
-                conc += "  "
+                conc += " " * chardnum
 
         out += f"{conc}\n"
 
-    print(out, f"printing [ Z=Z^N + C ] with N = {exp}")
+    print(out, f"printing [ Z=Z^N + C ] with N = {str(exp)[0:4] if exp < 10 else str(exp)[0:5]}")
 
 
 def main():
@@ -62,11 +65,22 @@ def main():
 
     if iterate:
         step = 0.05
-        arr = np.arange(1, exp + step, step)
+        arr = np.arange(1, exp + step, step) 
 
         try:
             for i in arr:
+
                 mandelbrot(i)
+
+                if i > 1 and i < 8 and round(i, 2).is_integer():
+                    time.sleep(1)
+                
+                elif i <= 3:
+                    time.sleep(0.03)
+
+                else:
+                    time.sleep(0.01)
+
 
                 if i != arr[-1]:
                     print("\x1b[H")
